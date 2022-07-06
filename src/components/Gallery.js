7874051a2a204c820281f3be0255c695dcbd936e
Salim <79255969+Salim255@users.noreply.github.com>
2022-07-06 10:35:23 +0200

    Header part-1

---
+++ src/components/Gallery.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div className='gallery'> 
+      <di>
+      Gallery
+      </di>
+      <di>
+      Gallery
+      </di>
+      <di>
+      Gallery
+      </di>
+    </div>
+  )
+}
+
+export default Gallery
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+       
+            <ul className='nav'>
+                    <li className='nav__item'><a href='#!' className='nav__link'>thomas</a></li>
+                    <li  className='nav__item'><a href='#!' className='nav__link'>about us</a> </li>
+                    <li  className='nav__item'><a href='#!' className='nav__link'>contact</a></li>
+            </ul>
+       
+    </div>
+  )
+}
